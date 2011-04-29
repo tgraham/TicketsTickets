30ebbbc2bfdb09ce9c9ed08880957f2689d68ebb
Travis Graham <t@tgraham.us>
2011-04-28 22:55:17 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketsTickets::Application.config.secret_token = '4c3ceff857677f64cdf8ff295e44c683d621428ddc352986797e199a5c9062c33c8ca00a31a260d4136ae530901d4c4e3239af3d3d64c33ee7c3a268e9baba85'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TicketsTickets::Application.config.session_store :cookie_store, :key => '_TicketsTickets_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TicketsTickets::Application.config.session_store :active_record_store
