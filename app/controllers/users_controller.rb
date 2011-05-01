d849fd82dcd8f54efe89bf1f501faefe0fb89149
Travis Graham <t@tgraham.us>
2011-04-30 22:13:18 -0400

    Rearranging routes updating locales to add login to error message moved input fields below labels added convenience name method to use

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-
+  
   def show
     @user = User.where(:slug => params[:id]).first
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,15 @@ class User
   field :slug, type: String
   
   # Setup accessible (or protected) attributes for your model
-  #attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
- 
- private 
-  def generate_slug
-    self.slug = login.parameterize
-  end
+  #attr_accessible :first_name, :last_name, :login, :slug, :email, :password, :password_confirmation, :remember_me
+
+  def name
+    self.first_name + ' ' + self.last_name
+  end  
+  
+  private 
+    def generate_slug
+      self.slug = login.parameterize
+    end
 
 end
