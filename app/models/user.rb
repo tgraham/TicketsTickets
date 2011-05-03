ee84d311294c296d786d3caab5655bf952818d7c
Evan Sparkman <esparkman@esdezines.com>
2011-05-02 22:11:04 -0400

    Logins working via username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
   include Mongoid::Slug
-  
-  before_create :create_login
-  
+    
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -22,23 +20,8 @@ class User
 
 protected
   
-  def create_login             
-    email = self.email.split(/@/)
-    login_taken = User.where( :login => email[0]).first 
-    unless login_taken
-      self.login = email[0]
-    else	
-      self.login = self.email
-    end	       
-  end
-  
-  # def self.find_for_database_authentication(conditions)
-  #   login = conditions.delete(:login)
-  #   self.any_of({ :username => login }, { :email => login }).first
-  # end
-
   def self.find_for_database_authentication(conditions)
-    self.where(:login => conditions[:email]).first || self.where(:email => conditions[:email]).first
+    login = conditions.delete(:login)
+    self.any_of({ :username => login }, { :email => login }).first
   end
-  
 end
