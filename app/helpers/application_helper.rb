93b69e7f8fea0c20ffb2d114560b5c5a004e7fc7
Travis Graham <t@tgraham.us>
2011-05-05 21:21:29 -0400

    Layout changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def avatar_url(user)  
+    default_url = "#{root_url}images/user_32.png"  
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase  
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=32&d=#{CGI.escape(default_url)}"  
+  end
 end
