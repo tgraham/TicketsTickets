19221310555c2c2683c2e146dc57a77126f1bec8
Travis Graham <t@tgraham.us>
2011-05-08 22:47:00 -0400

    Add a welcome title to the home static view Fix edit button for ticket show view adding active support inflections

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
+require "active_support/core_ext/string/inflections"
 require 'mongoid/railtie'
 
 # If you have a Gemfile, require the gems listed there, including any gems
